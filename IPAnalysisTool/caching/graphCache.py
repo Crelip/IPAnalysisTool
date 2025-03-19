4fde5bab5d10d357a391b2beb0517859401d9c99
Filip Hostak <hostak7@uniba.sk>
2025-03-19 16:32:59 +0100

    Added path indexing to graphCache.py

--- IPAnalysisTool/caching/graphCache.py
+++ IPAnalysisTool/caching/graphCache.py
@@ -19,7 +19,7 @@ from ..util.databaseUtil import connectToRemoteDB
 from json import dumps
 from sortedcontainers import SortedSet
 
-def isNondecreasingArray(arr):
+def is_nondecreasing_array(arr):
     size = len(arr)
     for i in range(1, size):
         if arr[i] < arr[i - 1]:
@@ -27,92 +27,95 @@ def isNondecreasingArray(arr):
     return True
 
 # Gets the earliest and latest date in the database
-def getDatabaseRange() -> Tuple[datetime.date, datetime.date]:
+def get_database_range() -> Tuple[datetime.date, datetime.date]:
     # Database connection setup
-    remConn, remCur = connectToRemoteDB()
+    rem_conn, rem_cur = connectToRemoteDB()
 
     # Get the earliest date
-    remCur.execute("SELECT MIN(t_date) FROM topology")
-    record = remCur.fetchone()
-    earliestDate = record[0]
+    rem_cur.execute("SELECT MIN(t_date) FROM topology")
+    record = rem_cur.fetchone()
+    earliest_date = record[0]
 
     # Get the latest date
-    remCur.execute("SELECT MAX(t_date) FROM topology")
-    record = remCur.fetchone()
-    latestDate = record[0]
+    rem_cur.execute("SELECT MAX(t_date) FROM topology")
+    record = rem_cur.fetchone()
+    latest_date = record[0]
 
-    remCur.close()
-    remConn.close()
+    rem_cur.close()
+    rem_conn.close()
 
-    return earliestDate, latestDate
+    return earliest_date, latest_date
 
 # Generates a graph based on all data from start date to end date
-def generateIntervalData(start, end, remCur, dataFolder : str, verbose : bool, weightedEdges : bool = False, collectMetadata : bool = False):
+def generate_interval_data(start, end, rem_cur, data_folder : str, verbose : bool, weighted_edges : bool = False, collect_metadata : bool = False):
 
     # Adding a node to the graph
-    def addNode(g, address, times, i, endpoint):
-        if address not in addressToVertex:
+    def add_node(g, address, times, i, endpoint):
+        if address not in address_to_vertex:
             node = g.add_vertex()
-            addressToVertex[address] = node
-            VertexToAddress[node] = address
-            ipAddress[node] = address
-            minNodeDistance[node] = times[i]
-            maxNodeDistance[node] = times[i]
-            if collectMetadata: nodeProperties[node] = who.lookup(address)
+            address_to_vertex[address] = node
+            vertex_to_address[node] = address
+            ip_address[node] = address
+            min_node_distance[node] = times[i]
+            max_node_distance[node] = times[i]
+            if collect_metadata: node_properties[node] = who.lookup(address)
         else:
-            node = addressToVertex[address]
-            if times[i] < minNodeDistance[node]: minNodeDistance[node] = times[i]
-            if times[i] > maxNodeDistance[node]: maxNodeDistance[node] = times[i]
+            node = address_to_vertex[address]
+            if times[i] < min_node_distance[node]: min_node_distance[node] = times[i]
+            if times[i] > max_node_distance[node]: max_node_distance[node] = times[i]
         if address == endpoint:
-            positionInRoute[node] = 2
+            position_in_route[node] = 2
         return node
 
     start = datetime.strftime(start, '%Y-%m-%d')
     end = datetime.strftime(end, '%Y-%m-%d')
 
     g = Graph(directed=True)
-    traversalsNum = g.new_edge_property("int")
-    ipAddress = g.new_vertex_property("string")
-    positionInRoute = g.new_vertex_property("int") # 1 - start, 2 - end
-    nodeDistances = g.new_vertex_property("vector<double>")
-    minNodeDistance = g.new_vertex_property("float")
-    maxNodeDistance = g.new_vertex_property("float")
-    avgNodeDistance = g.new_vertex_property("float")
-    nodeProperties = g.new_vertex_property("string")
+    traversals_num = g.new_edge_property("int")
+    ip_address = g.new_vertex_property("string")
+    position_in_route = g.new_vertex_property("int") # 1 - start, 2 - end
+    node_distances = g.new_vertex_property("vector<double>")
+    min_node_distance = g.new_vertex_property("float")
+    max_node_distance = g.new_vertex_property("float")
+    avg_node_distance = g.new_vertex_property("float")
+    node_properties = g.new_vertex_property("string")
 
-    g.edge_properties['traversals'] = traversalsNum
-    g.vertex_properties['ip'] = ipAddress
-    g.vertex_properties['positionInRoute'] = positionInRoute
-    g.vertex_properties['nodeDistances'] = nodeDistances
-    g.vertex_properties['minDistance'] = minNodeDistance
-    g.vertex_properties['maxDistance'] = maxNodeDistance
-    g.vertex_properties['avgDistance'] = avgNodeDistance
-    g.vertex_properties['properties'] = nodeProperties
+    g.edge_properties['traversals'] = traversals_num
+    g.vertex_properties['ip'] = ip_address
+    g.vertex_properties['positionInRoute'] = position_in_route
+    g.vertex_properties['nodeDistances'] = node_distances
+    g.vertex_properties['minDistance'] = min_node_distance
+    g.vertex_properties['maxDistance'] = max_node_distance
+    g.vertex_properties['avgDistance'] = avg_node_distance
+    g.vertex_properties['properties'] = node_properties
+
+    g.vp["routes"] = g.new_vertex_property("vector<int>")
+    g.ep["routes"] = g.new_edge_property("vector<int>")
 
     who = WhoIs()
-    addressToVertex = {}
-    VertexToAddress = {}
+    address_to_vertex = {}
+    vertex_to_address = {}
 
-    if weightedEdges:
-        edgeWeights = g.new_edge_property("vector<double>")
-        minEdgeWeight = g.new_edge_property("float")
-        avgEdgeWeight = g.new_edge_property("float")
-        maxEdgeWeight = g.new_edge_property("float")
-        g.edge_properties['weights'] = edgeWeights
-        g.edge_properties['avgWeight'] = avgEdgeWeight
-        g.edge_properties['maxWeight'] = maxEdgeWeight
-        g.edge_properties['minWeight'] = minEdgeWeight
+    if weighted_edges:
+        edge_weights = g.new_edge_property("vector<double>")
+        min_edge_weight = g.new_edge_property("float")
+        avg_edge_weight = g.new_edge_property("float")
+        max_edge_weight = g.new_edge_property("float")
+        g.edge_properties['weights'] = edge_weights
+        g.edge_properties['avgWeight'] = avg_edge_weight
+        g.edge_properties['maxWeight'] = max_edge_weight
+        g.edge_properties['minWeight'] = min_edge_weight
 
     # Adding starting IP address
-    startingaddress = "localhost"
+    starting_address = "localhost"
     node = g.add_vertex()
-    addressToVertex[startingaddress] = node
-    VertexToAddress[node] = startingaddress
-    ipAddress[node] = startingaddress
-    positionInRoute[node] = 1
-    nodeDistances[node] = [0]
+    address_to_vertex[starting_address] = node
+    vertex_to_address[node] = starting_address
+    ip_address[node] = starting_address
+    position_in_route[node] = 1
+    node_distances[node] = [0]
 
-    remCur.execute(f"""
+    rem_cur.execute(f"""
                     SELECT t_route, t_roundtrip, t_date FROM topology t JOIN non_reserved_ip n ON n.ip_addr = t.ip_addr
                        WHERE NOT ('0.0.0.0/32' = ANY(t_route))
                        AND t_status = 'C'
@@ -121,87 +124,93 @@ def generateIntervalData(start, end, remCur, dataFolder : str, verbose : bool, w
                        AND t_hops > 1
 """)
 
-    existingEdges = {}
-    routeDates = SortedSet()
-    for record in remCur:
+    existing_edges = {}
+    route_dates = SortedSet()
+
+    for route_index, record in enumerate(rem_cur):
         route = record[0]
         times = record[1]
-        if weightedEdges and not isNondecreasingArray(times): continue
+        if weighted_edges and not is_nondecreasing_array(times): continue
         date = record[2]
         endpoint = route[-1].split("/")[0]
         route_length = len(route)
+        g.vp.routes[address_to_vertex["localhost"]].append(route_index)
         for i in range(route_length):
-            if i == 0: src, dest = startingaddress, route[i]
+            if i == 0: src, dest = starting_address, route[i]
             else: src, dest = route[i - 1], route[i]
 
             if not(src == '0.0.0.0/32' or dest == '0.0.0.0/32') and src != dest:
-                srcAddress = src.split("/")[0]
-                destAddress = dest.split("/")[0]
+                src_address = src.split("/")[0]
+                dest_address = dest.split("/")[0]
 
                 # If the source address isn't in graph yet, add it
-                if(srcAddress not in addressToVertex):
-                    srcNode = addNode(g, srcAddress, times, i - 1, endpoint)
-                    addressToVertex[srcAddress] = srcNode
+                if(src_address not in address_to_vertex):
+                    src_node = add_node(g, src_address, times, i - 1, endpoint)
+                    address_to_vertex[src_address] = src_node
                 # If the destination address isn't in graph yet, add it
-                if(destAddress not in addressToVertex):
-                    destNode = addNode(g, destAddress, times, i, endpoint)
-                    addressToVertex[destAddress] = destNode
+                if(dest_address not in address_to_vertex):
+                    dest_node = add_node(g, dest_address, times, i, endpoint)
+                    address_to_vertex[dest_address] = dest_node
 
                 # Add edge if it doesn't exist
-                if (srcAddress, destAddress) not in existingEdges.keys():
-                    edge = g.add_edge(addressToVertex[srcAddress], addressToVertex[destAddress])
-                    existingEdges[(srcAddress, destAddress)] = edge
-                    traversalsNum[edge] = 1
-                    if weightedEdges:
-                        minEdgeWeight[edge] = times[i] - (times[i - 1] if i > 0 else 0)
+                if (src_address, dest_address) not in existing_edges.keys():
+                    edge = g.add_edge(address_to_vertex[src_address], address_to_vertex[dest_address])
+                    existing_edges[(src_address, dest_address)] = edge
+                    traversals_num[edge] = 1
+                    if weighted_edges:
+                        min_edge_weight[edge] = times[i] - (times[i - 1] if i > 0 else 0)
                 # Update edge's weights if it does exist
                 else:
-                    edge = existingEdges[(srcAddress, destAddress)]
+                    edge = existing_edges[(src_address, dest_address)]
                     # Increment number of traversals
-                    traversalsNum[edge] += 1
+                    traversals_num[edge] += 1
 
                 # Add distance to node
-                nodeDistances[destNode].append(times[i])
+                node_distances[dest_node].append(times[i])
 
-                if weightedEdges:
+                # Add route index to edge and vertex
+                g.ep.routes[edge].append(route_index)
+                g.vp.routes[dest_node].append(route_index)
+
+                if weighted_edges:
                     edgeWeight = times[i] - (times[i - 1] if i > 0 else 0)
-                    edgeWeights[edge].append(edgeWeight)
-                    if edgeWeight > float(maxEdgeWeight[edge]): maxEdgeWeight[edge] = edgeWeight
-                    if edgeWeight < float(minEdgeWeight[edge]): minEdgeWeight[edge] = edgeWeight
+                    edge_weights[edge].append(edgeWeight)
+                    if edgeWeight > float(max_edge_weight[edge]): max_edge_weight[edge] = edgeWeight
+                    if edgeWeight < float(min_edge_weight[edge]): min_edge_weight[edge] = edgeWeight
 
         # Check if we had the date before
         date = date.date()
-        if date not in routeDates:
-            routeDates.add(date)
+        if date not in route_dates:
+            route_dates.add(date)
 
-    if weightedEdges:
+    if weighted_edges:
         for e in g.edges():
-            avgEdgeWeight[e] = sum(edgeWeights[e]) / len(edgeWeights[e])
+            avg_edge_weight[e] = sum(edge_weights[e]) / len(edge_weights[e])
 
     metadata = g.new_graph_property("string")
     g.gp["metadata"] = metadata
     g.gp["metadata"] = dumps({"date": start,
-                              "routeDates": [getDateString(date) for date in routeDates],
-                              "weightedEdges": weightedEdges})
-    dataFolder = dataFolder + f"/{'base' if not weightedEdges else 'weighted'}"
-    if not os.path.exists(dataFolder): os.makedirs(dataFolder)
-    g.save(f"{dataFolder}/{start}.gt")
+                              "routeDates": [getDateString(date) for date in route_dates],
+                              "weightedEdges": weighted_edges})
+    data_folder = data_folder + f"/{'base' if not weighted_edges else 'weighted'}"
+    if not os.path.exists(data_folder): os.makedirs(data_folder)
+    g.save(f"{data_folder}/{start}.gt")
     if verbose:
-        print(f"Generated{' weighted' if weightedEdges else ''} graph for week starting with {start}.")
+        print(f"Generated{' weighted' if weighted_edges else ''} graph for week starting with {start}.")
         print(f"Number of vertices: {g.num_vertices()}\nNumber of edges: {g.num_edges()}")
     who.close()
 
 # For each week, generate a graph using generateOutput()
 def generateWeeklyData(start: datetime.date, end: datetime.date, verbose: bool, weightedEdges : bool = False, collectMetadata : bool = False):
     # Database connection setup
-    remConn, remCur = connectToRemoteDB()
+    rem_conn, rem_cur = connectToRemoteDB()
 
-    dataFolder : str = os.path.expanduser("~/.cache/IPAnalysisTool/graphs/week")
-    if not os.path.exists(dataFolder):
-        os.makedirs(dataFolder)
+    data_folder : str = os.path.expanduser("~/.cache/IPAnalysisTool/graphs/week")
+    if not os.path.exists(data_folder):
+        os.makedirs(data_folder)
 
     # Create a table of non-reserved IP addresses
-    remCur.execute("""CREATE TEMPORARY TABLE non_reserved_ip AS
+    rem_cur.execute("""CREATE TEMPORARY TABLE non_reserved_ip AS
                        (SELECT h.ip_addr AS ip_addr FROM hosts h WHERE
                        NOT (h.ip_addr <<= '0.0.0.0/8' 
                        OR h.ip_addr <<= '0.0.0.0/32' 
@@ -232,10 +241,10 @@ def generateWeeklyData(start: datetime.date, end: datetime.date, verbose: bool, 
     if verbose: print("Created non_reserved_ip table.")
     weeks = getWeekDates(start, end)
     for week in weeks:
-        generateIntervalData(week[0], week[0] + timedelta(days=7), remCur, dataFolder, verbose, weightedEdges)
+        generate_interval_data(week[0], week[0] + timedelta(days=7), rem_cur, data_folder, verbose, weightedEdges)
 
-    remCur.close()
-    remConn.close()
+    rem_cur.close()
+    rem_conn.close()
 
 def main(args = None):
     from argparse import ArgumentParser
@@ -256,7 +265,7 @@ def main(args = None):
     elif args.time:
         start, end = getWeek(datetime.strptime(args.time, "%Y-%m-%d"))
     else:
-        start, end = getDatabaseRange()
+        start, end = get_database_range()
 
     generateWeeklyData(start, end, args.verbose, args.weightedEdges, args.metadata)
 
