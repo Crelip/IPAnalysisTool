fa427bf40cff1766a8fc202ad83b7cb2020f3bbb
Filip Hostak <hostak7@uniba.sk>
2025-03-25 20:18:45 +0100

    Altered compare script + made a notebook for comparison.

--- ip_analysis_tool/compare.py
+++ ip_analysis_tool/compare.py
@@ -12,6 +12,10 @@ def compare_graphs_jaccard(g1: gt.Graph, g2: gt.Graph):
     edges2 = {edge_repr(e, g2.vp.ip) for e in g2.edges()}
     edges_in_intersection = edges1.intersection(edges2)
     edges_in_union = edges1.union(edges2)
+
+    vertices1 = set(g1.vp.ip[v] for v in g1.vertices())
+    vertices2 = set(g2.vp.ip[v] for v in g2.vertices())
+
     similarity = len(edges_in_intersection) / len(edges_in_union)
     return {
         "vertices_in_graph_1": g1.num_vertices(),
@@ -20,6 +24,8 @@ def compare_graphs_jaccard(g1: gt.Graph, g2: gt.Graph):
         "edges_in_graph_2": g2.num_edges(),
         "intersection_of_edges": len(edges_in_intersection),
         "union_of_edges": len(edges_in_union),
+        "intersection_of_vertices": len(vertices1.intersection(vertices2)),
+        "union_of_vertices": len(vertices1.union(vertices2)),
         "similarity": similarity
     }
 
