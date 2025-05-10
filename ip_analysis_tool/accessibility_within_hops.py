3d9eb7d47915779fde37ebcf922666ec1cabfe33
Filip Hostak <hostak7@uniba.sk>
2025-05-10 10:59:21 +0200

    Change handling graph accessibility within hops.

--- ip_analysis_tool/accessibility_within_hops.py
+++ ip_analysis_tool/accessibility_within_hops.py
@@ -6,11 +6,49 @@ from pandas import DataFrame
 def clamp(n, smallest, largest): return max(smallest, min(n, largest))
 
 def accessibility_within_hops(g: gt.Graph, get_ip_addresses = False) -> DataFrame:
+    """
+    Calculate the accessibility within hops for a given graph.
+    :param g: The graph to analyze.
+    :param get_ip_addresses: If True, include IP addresses in the output.
+    :return:
+    """
+    starting_node = g.vertex(0)
+    dist_dict = gt.shortest_distance(g, source=starting_node, directed=False)
+
+    max_dist = int(max(dist_dict)) if dist_dict else 0
+
+    # Group the reachable vertices by distance
+    ip_by_distance = defaultdict(list)
+    for v in g.vertices():
+        ip_by_distance[dist_dict[v]].append(g.vp.ip[v])
+    distance_records = []
+    for i in range(max_dist + 1):
+        ips = ip_by_distance[i]
+        count = len(ips)
+        cumulative_count = sum(len(ip_by_distance[j]) for j in range(i + 1))
+        record = {
+            "distance": i,
+            "count": count,
+            "percentage": count / g.num_vertices() * 100,
+            "cumulative_count": cumulative_count,
+            "cumulative_percentage": cumulative_count / g.num_vertices() * 100
+        }
+        if get_ip_addresses:
+            record["IPs"] = ips
+        distance_records.append(record)
+    return DataFrame(distance_records)
+
+def accessibility_within_hops_approx(g: gt.Graph, get_ip_addresses = False) -> DataFrame:
+    """
+    Calculate the accessibility within hops for a given graph. This is an approximation of the actual distances where it's simply collecting the recorded distances from the data, therefore it's faster, but sometimes less accurate.
+    :param g: The graph to analyze.
+    :param get_ip_addresses: If True, include IP addresses in the output.
+    :return:
+    """
     distances = g.vp["hop_distance"]
     dist_list = list(distances)
 
-    reachable_distances = [d for d in dist_list]
-    max_dist = int(max(reachable_distances)) if reachable_distances else 0
+    max_dist = int(max(dist_list)) if dist_list else 0
 
     # Group the reachable vertices by distance
     ip_by_distance = defaultdict(list)
